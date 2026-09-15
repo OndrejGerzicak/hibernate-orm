beb98a702b38daf0f4c47e49628f86e55adcbe83
Ondrej Gerzicak <ogerzicak@ibm.com>
2026-09-15 15:28:57 +0200

    test

--- docker_db.sh
+++ docker_db.sh
@@ -86,8 +86,7 @@ mysql_8_1() {
 
 mysql_8_2() {
     $CONTAINER_CLI rm -f mysql || true
-    $CONTAINER_CLI run --name mysql -e MYSQL_USER=hibernate_orm_test -e MYSQL_PASSWORD=hibernate_orm_test -e MYSQL_ROOT_PASSWORD=hibernate_orm_test -e MYSQL_DATABASE=hibernate_orm_test -e MYSQL_ROOT_PASSWORD=hibernate_orm_test -p3306:3306 -d ${DB_IMAGE_MYSQL_8_2:-docker.io/mysql:8.2.0} --character-set-server=utf8mb4 --collation-server=utf8mb4_0900_as_cs --skip-character-set-client-handshake --log-bin-trust-function-creators=1 --lower_case_table_names=2
-    # Give the container some time to start
+    $CONTAINER_CLI run --name mysql -e MYSQL_USER=hibernate_orm_test -e MYSQL_PASSWORD=hibernate_orm_test -e MYSQL_ROOT_PASSWORD=hibernate_orm_test -e MYSQL_DATABASE=hibernate_orm_test -e MYSQL_ROOT_PASSWORD=hibernate_orm_test -p3306:3306 -d ${DB_IMAGE_MYSQL_8_2:-docker.io/mysql:8.2.0} --character-set-server=utf8mb4 --collation-server=utf8mb4_0900_as_cs --init-connect="SET character_set_client=utf8mb4;SET character_set_results=utf8mb4;SET character_set_connection=utf8mb4;SET collation_connection=utf8mb4_0900_as_cs;" --log-bin-trust-function-creators=1 --lower_case_table_names=2 --innodb_native_foreign_keys    # Give the container some time to start
     OUTPUT=
     n=0
     until [ "$n" -ge 5 ]
